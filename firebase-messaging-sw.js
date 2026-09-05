82bd472b159e857f810da4c5f9e8658c3777f804
seongwoo <berkiss231@naver.com>
2026-09-05 20:56:27 +0900

    deploy 2026-09-05T11:52:47.506968+00:00

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,5 @@
+/* 싼타임 웹 푸시 서비스워커 — 생성 파일(hotdeal.web.webpush), 직접 수정 금지 */
+importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js');
+firebase.initializeApp({"apiKey":"AIzaSyBYGCf2XiFP-uy5x3b-Z_eh_RpR6u-yDjQ","projectId":"ssantime-naver","messagingSenderId":"1009738371231","appId":"1:1009738371231:web:0a434891055eee1a7d12aa"});
+firebase.messaging();
